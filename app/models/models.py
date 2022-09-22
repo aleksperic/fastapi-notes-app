5bc3568c5c65b9d1e71294d803fd6cdc6aa1d4e6
Aleksandar <aca.peric93@gmail.com>
2022-09-22 23:32:48 +0200

    db models

---
+++ app/models/models.py
@@ -0,0 +1,27 @@
+from sqlalchemy import Column, Integer, String, Boolean, DateTime, ForeignKey
+from sqlalchemy.orm import relationship
+from models.database import Base
+
+class User(Base):
+
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True)
+    password = Column(String)
+    email = Column(String)
+
+    posts = relationship('Note', back_populates='users')
+
+class Note(Base):
+
+    __tablename__ = 'posts'
+
+    id = Column(Integer, primary_key=True, index=True)
+    titile = Column(String)
+    body = Column(String)
+    active = Column(Boolean(default=True))
+    created = Column(DateTime(timezone=True))
+    user_id = Column(Integer, ForeignKey('users.id'))
+
+    users = relationship('User', back_populates='posts')
