5a98d28fbc25651276fdc27a24e86c56633dc61d
Aleksandar <aca.peric93@gmail.com>
2022-10-03 23:39:59 +0200

    routers refactoring

--- app/routers/notes.py
+++ app/routers/notes.py
@@ -11,35 +11,52 @@ from datetime import datetime
 
 
 router = APIRouter(
-    prefix='/notes',
-    tags=['Notes'],
-    #dependencies=[Depends(get_token_header)],
-    responses={404: {'description': 'Not found'}}
-    )
+                    prefix='/notes',
+                    tags=['Notes'],
+                    responses={404: {'description': 'Not found'}}
+                 )
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 
-@router.post('/', response_model=schemas.NoteShow, status_code=status.HTTP_201_CREATED)
-def create_note(request: schemas.Note, current_user: schemas.UserShow = Depends(auth.get_current_user), db: Session = Depends(get_db)):
-    return notes.create_note(request, current_user, db)
-
-@router.get('/{id}', response_model=schemas.NoteShow, status_code=status.HTTP_200_OK)
-def get_note(id: int, db: Session = Depends(get_db)):
-    return notes.get_note(id, db)
-
-@router.get('/', response_model=List[schemas.NoteShow], status_code=status.HTTP_200_OK)
-def get_notes(db: Session = Depends(get_db), current_user: schemas.UserShow = Depends(auth.get_current_user)):
-    return notes.get_notes(db, current_user)
-
-@router.get('/', response_model=List[schemas.NoteShow], status_code=status.HTTP_200_OK)
-def get_public_notes(username: str, db: Session = Depends(get_db)):
+@router.get('/{username}', response_model=List[schemas.UserPublicNotes], status_code=status.HTTP_200_OK)
+def get_public_notes(   username: str,
+                        db: Session = Depends(get_db)
+                    ):
     return notes.get_public_notes(username, db)
 
+@router.post('/', response_model=schemas.NoteShow, status_code=status.HTTP_201_CREATED)
+def create_note(request: schemas.Note,
+                current_user: schemas.UserShow = Depends(auth.get_current_user),
+                db: Session = Depends(get_db)
+                ):
+    return notes.create_note(request, current_user, db)
+
+@router.get('/note/{id}', response_model=schemas.NoteShow, status_code=status.HTTP_200_OK)
+def get_note(   id: int,
+                db: Session = Depends(get_db),
+                current_user: schemas.UserShow = Depends(auth.get_current_user)
+            ):
+    return notes.get_note(id, db, current_user)
+
+@router.get('/', response_model=List[schemas.NoteShow], status_code=status.HTTP_200_OK)
+def get_notes(  db: Session = Depends(get_db),
+                current_user: schemas.UserShow = Depends(auth.get_current_user)
+                ):
+    return notes.get_notes(db, current_user)
+
 @router.put('/{id}', status_code=status.HTTP_202_ACCEPTED)
-def update_note(id: int, active: bool, request: schemas.NoteUpdate, db: Session = Depends(get_db)):
-    return notes.update_note(id, active, request, db)
+def update_note(id: int,
+                public: bool,
+                request: schemas.NoteUpdate,
+                db: Session = Depends(get_db),
+                current_user: schemas.UserShow = Depends(auth.get_current_user)
+                ):
+    return notes.update_note(id, public, request, db, current_user)
 
 @router.delete('/{id}', status_code=status.HTTP_410_GONE)
-def delete_note(id: int, db: Session = Depends(get_db)):
-    return notes.delete_note(id, db)
+def delete_note(id: int,
+                db: Session = Depends(get_db),
+                current_user: schemas.UserShow = Depends(auth.get_current_user)
+                ):
+    return notes.delete_note(id, db, current_user)
